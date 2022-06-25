3dae0728883f129286ae69407c0f44fcd569ecba
bhaveshabcd1234bhavu@gmail.com <bhaveshabcd1234bhavu@gmail.com>
2022-06-25 19:24:15 +0530

    need

---
+++ main.js
@@ -0,0 +1,31 @@
+console.log('Hello World!');
+
+
+
+function gh(){
+
+var g  = document.getElementById('in');
+var y = document.getElementById('sr');
+
+
+g.classList.toggle("hgn");
+ y.classList.toggle("bgc");
+
+}
+
+function mn(){
+var g  = document.getElementById('hg');
+var k  = document.getElementById('meno_icon');
+
+g.classList.toggle("bh");
+
+k.classList.toggle("menu_icon");
+
+/*var y = document.getElementById('bo');var u = document.getElementById('ob');
+*/
+/*y.classList.toggle("bgo");
+u.classList.toggle("buo");*/
+}
+
+
+
